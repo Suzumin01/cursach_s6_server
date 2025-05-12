ee8a0af42f4abbf955154ad5b6f78e2c63e10f26
Nikita Smolnikov <151205132+Suzumin01@users.noreply.github.com>
2025-05-12 08:12:44 +0300

    Initial commit

---
+++ kinoplay/build.gradle.kts
@@ -0,0 +1,37 @@
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.10"
+    id("io.ktor.plugin") version "3.1.2"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "com.example.ApplicationKt"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.core)
+
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("com.h2database:h2:2.2.224")
+
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
